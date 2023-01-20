1aa142f8b2b5a4b8d38cac5e1f1ad7ddca632123
myhency <hency.yeo@gmail.com>
2023-01-20 13:39:25 +0900

    feat: bottom navigation 추가

--- app/src/main/java/com/io/play/navigationpractice/MainActivity.kt
+++ app/src/main/java/com/io/play/navigationpractice/MainActivity.kt
@@ -2,10 +2,52 @@ package com.io.play.navigationpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.io.play.navigationpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener {_, destination, _ ->
+            Log.d("Bottom", "id = ${destination.id}, label = ${destination.label}")
+        }
+
+        bottomNavigation.setOnItemSelectedListener {
+            replaceFragment(
+                when (it.itemId) {
+                    R.id.menu_home -> HomeFragment()
+                    R.id.menu_market -> MarketFragment()
+                    else -> PlaylistFragment()
+                }
+            )
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.navHostFragment.id, fragment)
+            .commit()
     }
 }
